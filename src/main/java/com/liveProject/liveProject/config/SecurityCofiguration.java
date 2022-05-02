d56dcc46ebb5b6cca7ceab5dd4ba2de04b6debdb
atumar <almustapha.tukur@decagon.dev>
2022-05-02 17:30:48 +0100

    with static user

--- src/main/java/com/liveProject/liveProject/config/SecurityCofiguration.java
+++ src/main/java/com/liveProject/liveProject/config/SecurityCofiguration.java
@@ -1,28 +1,39 @@
 package com.liveProject.liveProject.config;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 
 @EnableWebSecurity
 public class SecurityCofiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("user")
-                .roles("USER")
-                .and()
-                .withUser("admin")
-                .password("admin")
-                .roles("ADMIN");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(
+                        User.withUsername("user")
+                                .password("user")
+                                .roles("USER")
+                ).withUser(
+                        User.withUsername("admin")
+                                .password("admin")
+                                .roles("ADMIN")
+                );
     }
 
     @Override
